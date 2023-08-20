56d352306c776a68574735d4b063211fb8e73ebe
alphaprime7 <awesome.tingwei@outlook.com>
2023-08-20 09:15:53 -0500

    up

--- R/norm_tidy_dbf.R
+++ R/norm_tidy_dbf.R
@@ -84,7 +84,7 @@ min_max_norm <- function(x){
 #' @examples
 #' require(gh)
 #' qurl = "https://raw.github.com/AlphaPrime7/normfluodbf/main/data-raw/liposomes_214.dbf"
-#' gh(paste0('GET ', qurl), .destfile = "getwd()/data-raw/tmp.dbf", .overwrite = TRUE)
+#' gh(paste0('GET ', qurl), .destfile = paste0(getwd(),"/data-raw/temp.dbf"), .overwrite = TRUE)
 #' normalized_dbf <- norm_tidy_dbf(file= tmp.dbf)
 #'
 norm_tidy_dbf <- function(file = NULL, fun = NA, ...){
