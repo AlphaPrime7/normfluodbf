9e993a21d4204949e0a806bf8c5313fe9552d85e
alphaprime7 <awesome.tingwei@outlook.com>
2023-10-13 17:21:56 -0500

    up-just covering a few user scenarios.

--- R/norm_tidy_dbf.R
+++ R/norm_tidy_dbf.R
@@ -129,6 +129,8 @@ log_transformation <- function(x){
 #'
 #'
 #' @param file A string ("liposomes_xxx.dbf") if the file is found within the present working directory (pwd) OR a path pointing directly to a ".dbf" file, from FLUOstar experiments.
+#' @param norm_scale This parameter can taken in 'hundred', 'one', or 'z-score' which denotes the normalization type; Initialized as NULL.
+#' @param transformed This parameter can take in 'log' which denotes a logarithmic box-cox transformation; Initialized as NULL.
 #' @param fun A variable defined as NA, used for boolean expressions or manipulation.
 #' @param ... A container object that can be used to capture extra variables if needed.
 #'
@@ -140,51 +142,279 @@ log_transformation <- function(x){
 #'
 #' @export
 #' @note Re-nomenclature of norm_tidy_dbf to a more appropriate name that facilitates function utilization. Users can continue with the old name ("norm_tidy_dbf") but this is a better name in my opinion.
+#' It is important to know that if the user chooses to log transform the data then the program defaults to a min_max normalization with range between 0 and 1.
 #'
 #' @examples
 #' fpath <- system.file("extdata", "liposomes_214.dbf", package = "normfluodbf", mustWork = TRUE)
 #' normalized_dbf <- normfluordbf(file=fpath)
+#' normalized_dbf_scale100 <- normfluordbf(file=fpath, norm_scale = 'hundred')
 
-norm_tidy_dbf <- function(file = NULL, norm_method = NULL, transformed = NULL, fun = NA, ...){
+norm_tidy_dbf <- function(file = NULL, norm_scale = NULL, transformed = NULL, fun = NA, ...){
 
   if(!is.null(file)){
     x <- foreign::read.dbf(file=file, as.is = F)
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
+
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
+    }
+
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
+    }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], min_max_norm))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
+
   } else {
+
     warning("please enter a string for the .dbf file you want to normalize")
   }
 
-  y <- data.table::transpose(l=x)
-  rownames(y) <- colnames(x)
-  colnames(y) <- rownames(x)
-  colnames(y) <- paste0("a",rownames(x))
+  if(!is.null(file) && norm_scale == 'hundred'){
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
 
-  sample_col_names<- vector("list")
-  nofun <- is.na(fun)
-  for(j in y[1,]){
-    if(is.na(j) != nofun){
-      sample_col_names <- c(sample_col_names,j)
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
     }
-  }
 
-  nofun <- is.na(fun)
-  dirty_time <- y[,1]
-  dbf_time_column <- data.frame()
-  for(i in dirty_time){
-    if(is.na(i) != nofun && i != "t"){
-      dbf_time_column <- rbind(dbf_time_column,i)
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
     }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], min_max_norm_percent))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
+
+  } else if (!is.null(file) && norm_scale == 'one'){
+
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
+
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
+    }
+
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
+    }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], min_max_norm))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
+
+  } else if(!is.null(file) && norm_scale == 'z-score'){
+
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
+
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
+    }
+
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
+    }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], norm_z))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
+
+  } else if(!is.null(file) && norm_scale == 'one' && transformed == 'log'){
+
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
+
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
+    }
+
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
+    }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], log_transformation))
+    y <- as.data.frame(lapply(y[1:ncol(y)], min_max_norm))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
+
+  } else if(!is.null(file) && norm_scale == 'hundred' && transformed == 'log'){
+
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
+
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
+    }
+
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
+    }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], log_transformation))
+    y <- as.data.frame(lapply(y[1:ncol(y)], min_max_norm_percent))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
+
+  } else if(!is.null(file) && norm_scale == 'z-score' && transformed == 'log'){
+
+    y <- data.table::transpose(l=x)
+    rownames(y) <- colnames(x)
+    colnames(y) <- rownames(x)
+    colnames(y) <- paste0("a",rownames(x))
+
+    sample_col_names<- vector("list")
+    nofun <- is.na(fun)
+    for(j in y[1,]){
+      if(is.na(j) != nofun){
+        sample_col_names <- c(sample_col_names,j)
+      }
+    }
+
+    nofun <- is.na(fun)
+    dirty_time <- y[,1]
+    dbf_time_column <- data.frame()
+    for(i in dirty_time){
+      if(is.na(i) != nofun && i != "t"){
+        dbf_time_column <- rbind(dbf_time_column,i)
+      }
+    }
+    colnames(dbf_time_column) <- c('Time')
+
+    y[1:3,] <- NA
+    y <- y %>% drop_na()
+    y <- y[,-(1:2)]
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y <- as.data.frame(lapply(y[1:ncol(y)], log_transformation))
+    y <- as.data.frame(lapply(y[1:ncol(y)], norm_z))
+    colnames(y) <- sample_col_names
+    y <- cbind(y,dbf_time_column)
+    y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
+    y["Time"] = y[,"Time"] + 30
+
+    return(unique_identifier(y))
   }
-  colnames(dbf_time_column) <- c('Time')
-
-  y[1:3,] <- NA
-  y <- y %>% drop_na()
-  y <- y[,-(1:2)]
-  y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
-  y <- as.data.frame(lapply(y[1:ncol(y)], min_max_norm))
-  colnames(y) <- sample_col_names
-  y <- cbind(y,dbf_time_column)
-  y[, c(1:ncol(y))] <- sapply(y[, c(1:ncol(y))], as.numeric)
-  y["Time"] = y[,"Time"] + 30
-
-  return(unique_identifier(y))
 }
