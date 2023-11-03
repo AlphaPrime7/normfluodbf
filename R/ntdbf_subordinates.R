4b2ef3700f2d11a12a14d05ae400ca9e858d0b5e
alphaprime7 <awesome.tingwei@outlook.com>
2023-11-02 22:01:19 -0500

    up

--- R/ntdbf_subordinates.R
+++ R/ntdbf_subordinates.R
@@ -1,13 +1,15 @@
 #' Title: A function to append a unique identifier attribute to any data frame within the normfluodbf package.
+#'
 #' @description
 #' The function in the context of normfluodbf creates an attribute called Cycle_Number
 #' and appends this attribute to the cleaned or wrangled data frame derived from the dirty DBF file.
 #'
 #' @author Tingwei Adeck
 #'
-#' @param df A data frame with 1:n number of rows
+#' @param df A data frame with 1:n number of rows.
 #'
 #' @return A data frame with the Cycle_Number attribute appended to the end of the data frame.
+#'
 #' @export
 #'
 #' @seealso [normfluodat()], [norm_tidy_dbf()], [normfluordbf()], [generic_identifier()]
@@ -30,15 +32,18 @@ unique_identifier <- function(df){
 }
 
 #' Title: A function to append a generic identifier attribute to any data frame, but users supply a name for said attribute.
+#'
 #' @description
 #'A function that creates an attribute of seq(numrows) with a step size of 1, where the user provides the attribute name.
 #'
 #' @author Tingwei Adeck
 #'
 #' @param numrows The number of rows the user intends to have in the created data frame.
+#'
 #' @param col_name The desired attribute name.
 #'
 #' @return A user-named single attribute data frame with nrow = numrows.
+#'
 #' @export
 #'
 #' @examples generic_identifier(40, col_name="Cycle_No")
@@ -50,74 +55,88 @@ generic_identifier <- function(numrows, col_name){
   return(df)
 }
 
-#' Title: A function that performs Min-Max normalization(0-1 range) of attributes that require normalization
+#' Title: Min-Max normalization on a 0-1 scale.
 #'
-#' @author Tingwei Adeck (Adapted from Statology)
-#' @param x Column or attribute values passed into the min-max normalization function
+#' @author Tingwei Adeck
 #'
-#' @return A normalized value (between 0 and 1) when used as a standalone function, or a normalized attribute(s) when used with lapply.
+#' @param x Attribute value(s).
+#'
+#' @return A normalized value (between 0 and 1) when applied to a single value or a normalized attribute with values between 0 and 1.
+#'
 #' @export
-#' @note The lapply function is required to apply the function across several columns in a data set.
+#'
+#' @note The lapply function is required to apply this function across several attributes.
 #'
 #'
 #' @examples test_df <- as.data.frame(c(seq(40)))
 #' colnames(test_df) <- "test"
 #' test_df_norm <- lapply(test_df[1:ncol(test_df)], min_max_norm)
+#'
 #' @references https://www.statology.org/how-to-normalize-data-in-r/
 
 min_max_norm <- function(x){
   (x - min(x)) / (max(x) - min(x))
 }
 
-#' Title: A function that performs Min-Max normalization (0-100 range) of attributes that require normalization
+#' Title: Min-Max normalization on a 0-100 scale.
 #'
 #' @author Tingwei Adeck
-#' @param x Column or attribute values passed into the min-max normalization function
 #'
-#' @return A normalized value (between 0 and 100) when used as a standalone function, or a normalized attribute(s) when used with lapply.
+#' @param x Attribute value(s).
+#'
+#' @return A normalized value (between 0 and 100) when applied to a single value or a normalized attribute with values between 0 and 100.
+#'
 #' @export
-#' @note The lapply function is required to apply the function across several columns in a data set.
+#'
+#' @note The lapply function is required to apply this function across several attributes.
 #'
 #'
 #' @examples test_df <- as.data.frame(c(seq(40)))
 #' colnames(test_df) <- "test"
 #' test_df_norm <- lapply(test_df[1:ncol(test_df)], min_max_norm_percent)
+#'
 #' @references https://www.statology.org/how-to-normalize-data-in-r/
 
 min_max_norm_percent <- function(x){
   ((x - min(x)) / (max(x) - min(x))) * 100
 }
 
-#' Title: A function that performs Z-score standardization (or normalization) of attributes
+#' Title: Z-score standardization or normalization function.
 #'
 #' @author Tingwei Adeck
-#' @param x Column or attribute values passed into the Z-standardization function
 #'
-#' @return Attribute(s) with values that have mean 0 and standard deviation 1
+#' @param x Attribute value(s).
+#'
+#' @return A standardized value (Z = N (0,1)) when applied to a single value or a standardized attribute with values (Z = N (0,1)).
+#'
 #' @export
-#' @note The lapply function is required to apply the function across several columns in a data set.
+#'
+#' @note The lapply function is required to apply this function across several attributes.
 #'
 #'
 #' @examples test_df <- as.data.frame(c(seq(40)))
 #' colnames(test_df) <- "test"
 #' test_df_norm <- lapply(test_df[1:ncol(test_df)], norm_z)
+#'
 #' @references https://www.statology.org/how-to-normalize-data-in-r/
 
 norm_z <- function(x){
   (x - mean(x)) / sd(x)
 }
 
-#' Title: A function that performs decimal scaling (not normalization) of attributes
+#' Title: A decimal scaling function (a machine learning tool).
 #'
 #' @author Tingwei Adeck
-#' @param x Column or attribute values passed into the decimal scaling function
 #'
-#' @return Attribute(s) with values that have the decimal place moved to facilitate machine learning
+#' @param x Attribute value(s).
+#'
+#' @return A decimal scaled value when applied to a single value or a decimal scaled attribute(s).
+#'
 #' @export
-#' @note The lapply function is required to apply the function across several columns in a data set.
-#' This is NOT a normalization function because the data still exist on a sliding scale.
-#' This function is here for demonstration purposes and should be used for exercise as it is here for educational purposes for the inventor of the package.
 #'
+#' @note The lapply function is required to apply this function across several attributes.
+#' This is NOT a normalization function, so data obtained from the decimal scaling function
+#' exists on a sliding scale and SHOULD NOT be used for meaningful analysis.
 #'
 #' @examples test_df <- as.data.frame(c(seq(40)))
 #' colnames(test_df) <- "test"
@@ -130,33 +149,38 @@ decimal_scaling <- function(x){
   x/(10^power)
 }
 
-#' Title: A function that performs log transformation of data
+#' Title: A log transformation function.
 #'
 #' @author Tingwei Adeck
-#' @param x Column or attribute values passed into the Z-standardization function
 #'
-#' @return Log transformed attributes
+#' @param x Attribute value(s).
+#'
+#' @return A log-transformed value when applied to a single value or an attribute with log-transformed values.
+#'
 #' @export
-#' @note The lapply function is required to apply the function across several columns in a data set.
+#'
+#' @note The lapply function is required to apply this function across several attributes.
 #'
 #'
 #' @examples test_df <- as.data.frame(c(seq(40)))
 #' colnames(test_df) <- "test"
 #' test_df_norm <- lapply(test_df[1:ncol(test_df)], log_transformation)
+#'
 #' @references https://www.statology.org/how-to-normalize-data-in-r/
 
 log_transformation <- function(x){
   log(x)
 }
 
-#' Title: Round attribute elements or attributes
+#' Title: A value rounding function.
+#'
 #' @description
-#' Round an attribute element or attribute.
+#' Round attribute values to three decimal places.
 #'
 #'
-#' @param x An attribute element that needs to be rounded up
+#' @param x Attribute value(s).
 #'
-#' @return A rounded up element OR a rounded up attribute when assisted by lapply
+#' @return A rounded value with three decimal places when applied to a single value or an attribute with log-transformed values.
 #'
 #' @export
 #'
