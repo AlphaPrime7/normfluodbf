ba795b4f96ec50d28db535427d738e6d50769e9e
alphaprime7 <awesome.tingwei@outlook.com>
2023-11-04 05:12:34 -0500

    up

--- R/comma_cleaner.R
+++ R/comma_cleaner.R
@@ -1,22 +1,27 @@
-#' Title: A function to remove unwanted commas in a partially cleaned data frame
+#' Title: Comma Cleaner function.
+#'
 #' @description
-#' The function takes a dirty data frame (comma_df) derived from either the clean_odddat or clean_evendat functions.
-#' The resulting data frame is a cleaned data frame with numeric values.
+#' This modular function, in the context of this package, is responsible for removing commas
+#' from attribute(s) values. Removal of commas facilitates the conversion of attributes into the numeric class.
 #'
 #' @author Tingwei Adeck
-#' @param comma_df A dirty data frame (numeric values have commas at the end) with n number of rows
+#'
+#' @param comma_df A dirty data frame obtained from the FLUOstar DAT file.
 #'
 #'
-#' @return A new data frame with commas at the end of numbers taken out
+#' @return A clean data frame with numeric no-comma values for attribute(s).
+#'
 #' @export
-#' @note This function is a subordinate function and follows a sequence of actions. In this package, this function cannot be used as a standalone.
+#'
+#' @seealso [clean_odd_cc()], [clean_odddat_optimus()]
 #'
 #' @examples fpath <- system.file("extdata", "dat_1.dat", package = "normfluodbf", mustWork = TRUE)
 #' dat_df <- read.table(file=fpath)
 #' nocomma_dat <- comma_cleaner(dat_df)
+
 comma_cleaner <- function(comma_df){
   cols_to_becleaned <- c(colnames(comma_df))
-  comma_df[ , cols_to_becleaned] <- lapply(comma_df[ , cols_to_becleaned],  # Convert data
+  comma_df[ , cols_to_becleaned] <- lapply(comma_df[ , cols_to_becleaned],
                                            function(x){ as.numeric(as.character(gsub(",", "", x))) })
   return(as.data.frame(comma_df))
 
