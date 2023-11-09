3bd8226c004a71d58078318d7f18c4117ee00ab2
alphaprime7 <awesome.tingwei@outlook.com>
2023-11-09 16:20:19 -0600

    up

---
+++ doc/normfluodbf.R
@@ -0,0 +1,19 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "vignettes/",
+  out.width = "100%"
+)
+
+## ----echo=FALSE, results="hide", message=FALSE--------------------------------
+library("hexSticker")
+
+## ----out.width = '30%',out.height="20%",fig.cap="File Types",fig.show='hold',fig.align='center', echo=FALSE----
+knitr::include_graphics(c("img/dbf.png","img/dat.png"))
+#{R Chunk name, echo=FALSE, [...], fig.height=3,  dev='svg'}
+
+## ----out.width = '70%', echo=FALSE--------------------------------------------
+knitr::include_graphics("img/navab.png")
+#{R Chunk name, echo=FALSE, [...], fig.height=3,  dev='svg'}
+
