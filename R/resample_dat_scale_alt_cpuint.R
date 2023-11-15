be2b05f65c1665095e7e869438f9c348beedc529
alphaprime7 <awesome.tingwei@outlook.com>
2023-11-15 01:46:22 -0600

    up-wow

--- R/resample_dat_scale_alt_cpuint.R
+++ R/resample_dat_scale_alt_cpuint.R
@@ -1,7 +1,9 @@
 #' Title: A function to create an attribute or column for each sample loaded into the microplate wells.
 #'
 #' @description
-#' Creates a data frame where each sample loaded into the microplate wells has a separate attribute.
+#' A function that takes tuples or rows consisting of several samples and perform a
+#' putative resampling to yield another data frame with a separate attribute for each
+#' sample.
 #'
 #' @author Tingwei Adeck
 #'
@@ -9,44 +11,41 @@
 #' @param tnp A numeric value indicating the number of rows used. TNP is used as an acronym for Test, Negative, Positive.
 #' @param cycles A numeric value indicating the number of cycles selected by the user when running the FLUOstar instrument.
 #'
-#' @import stats
-#' @importFrom data.table transpose
-#'
 #' @return A new data frame where separated samples are assigned a separate attribute or column.
 #'
 #' @export
 #'
-#' @note This function builds on or scales-up  @seealso [resample_dat()], hence the suffix scale.
-#' This function is less optimized than @seealso [resample_dat_scale_optimus()].
-#'
-#' @seealso [resample_dat()]
+#' @seealso [resample_dat_alt()], [resample_dat_scale_alt()]
 #'
 #' @examples fpath <- system.file("extdata", "dat_4.dat", package = "normfluodbf", mustWork = TRUE)
 #' dat_df <- read.table(file=fpath)
 #' nocomma_dat <- clean_odddat_optimus(dat_df)
-#' resampled_scaled <- resample_dat_scale(nocomma_dat, tnp=3, cycles=40)
+#' resampled_scaled <- resample_dat_scale_alt_cpuint(nocomma_dat, tnp=3, cycles=40)
 
-resample_dat_scale <- function(df, tnp, cycles){
+resample_dat_scale_alt_cpuint <- function(df, tnp, cycles){
 
   suppressWarnings({
 
     col_list <- c()
     for(i in 1:ncol(df)){
-      col_list <- c( col_list, as.data.frame(df[,i]) )
+      n <- "a"
+      col_list <- c(col_list,assign(paste0(n, i), as.data.frame(df[,i])) )
     }
 
     j_vect <- c()
     for(j in col_list){
       j <- as.data.frame(j)
-      j_resampled <- resample_dat(j, tnp = tnp, cycles = cycles)
+      j_resampled <- resample_dat_alt(j, tnp = tnp, cycles = cycles)
       j_dfs <- as.data.frame(j_resampled)
       j_vect <- c(j_vect, j_dfs)
     }
 
     big_data = do.call(rbind, j_vect)
     big_data = as.data.frame(big_data)
-    big_data_t = data.table::transpose(l=big_data)
+    big_data_t = transpose(l=big_data)
+
     big_data_t <- big_data_t %>% dplyr::select_if(~ !any(is.na(.)))
+    #big_data_t <- big_data_t[ , colSums(is.na(big_data_t))==0]
 
     return(big_data_t)
 
