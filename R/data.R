c30ba67d3671f04b29008bdf39e5d6a844e7a138
alphaprime7 <awesome.tingwei@outlook.com>
2023-11-14 18:30:48 -0600

    up-wow

--- R/data.R
+++ R/data.R
@@ -45,22 +45,50 @@ NULL
 
 #' dat_1.
 #'
-#' FLUOstar .dat files used for creation of the update and unusable for data immediate data analysis.
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
 #'
 #'
 "dat_1"
 
 #' dat_2.
 #'
-#' FLUOstar .dat files used for creation of the update and unusable for data immediate data analysis.
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
 #'
 #'
 "dat_2"
 
 #' dat_3.
 #'
-#' FLUOstar .dat files used for creation of the update and unusable for data immediate data analysis. 
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
 #' This file is unique because it validates a major bug fix to ensure that users get the right output.
 #'
 "dat_3"
+
+#' dat_4.
+#'
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
+#' This file is unique because it validates a major bug fix to ensure that users get the right output.
+#'
+"dat_4"
+
+#' dat_5.
+#'
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
+#' This file is unique because it validates a major bug fix to ensure that users get the right output.
+#'
+"dat_5"
+
+#' dat_6.
+#'
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
+#' This file is unique because it validates a major bug fix to ensure that users get the right output.
+#'
+"dat_6"
+
+#' dat_7.
+#'
+#' FLUOstar .dat files used for creation of the update and unusable for immediate data analysis.
+#' This file is unique because it validates a major bug fix to ensure that users get the right output.
+#'
+"dat_7"
 
--- data-raw/dat_3.R
+++ data-raw/dat_3.R
@@ -1,3 +1,3 @@
 ## code to prepare `C:/Users/GrandProf/Downloads/Repos_4cleanup/Repositories_AP7/Active/normfluodbf/inst/extdata/dat_3.dat` dataset goes here
 
-usethis::use_data(C:/Users/GrandProf/Downloads/Repos_4cleanup/Repositories_AP7/Active/normfluodbf/inst/extdata/dat_3.dat, overwrite = TRUE)
+usethis::use_data("C:/Users/GrandProf/Downloads/Repos_4cleanup/Repositories_AP7/Active/normfluodbf/inst/extdata/dat_3.dat", overwrite = TRUE)
