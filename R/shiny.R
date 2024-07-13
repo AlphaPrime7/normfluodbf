6de3093205038bf11b62ab0a084c3d72c7ac6dda
alphaprime7 <awesome.tingwei@outlook.com>
2024-07-13 12:49:20 -0500

    Initial commit

---
+++ R/shiny.R
@@ -0,0 +1,21 @@
+#' Run the shiny App
+#' In addition to the functions provided in this package, the \code{normfluodbf} package
+#' also provides an interactive tool that can be used to analyze liposome flux assay data
+#' more easily. The tool will be launched in a web browser.
+#' @export
+launch <- function() {
+  shiny::runApp(system.file("shiny", package = "normfluodbf"),
+                display.mode = "normal",
+                launch.browser = TRUE)
+}
+
+#' Run the shiny App
+#' In addition to the functions provided in this package, the \code{normfluodbf} package
+#' also provides an interactive tool that can be used to analyze liposome flux assay data
+#' more easily. The tool will be launched in a web browser.
+#' @export
+demo <- function() {
+  shiny::runApp(system.file("demo", package = "normfluodbf"),
+                display.mode = "normal",
+                launch.browser = TRUE)
+}
