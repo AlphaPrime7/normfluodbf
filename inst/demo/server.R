ed5fbf662c70717e2a7d0404654a4d282997ed5c
alphaprime7 <awesome.tingwei@outlook.com>
2024-07-16 01:00:54 -0500

    Initial commit

--- inst/demo/server.R
+++ inst/demo/server.R
@@ -10,11 +10,56 @@ library(profvis)
 
 
 shinyServer(function(input, output, session) {
-  Sys.sleep(3)
+
+  Sys.sleep(1)
   observe({
     removeUI(selector = "#loading-content")
     shinyjs::show("main_nav")
   })
+
+  credentials <- shinyauthr::loginServer(
+    id = "login",
+    data = user_base,
+    user_col = user,
+    pwd_col = password,
+    log_out = reactive(logout_init())
+  )
+
+  logout_init <- reactiveVal(FALSE)
+
+  observeEvent(credentials()$user_auth, {
+    if (credentials()$user_auth) {
+      shinyjs::hide(id = "login")
+    } else {
+      shinyjs::show(id = "login")
+    }
+  })
+
+  output$app_ui <- renderUI({
+    req(credentials()$user_auth)
+    tagList(
+      navbarPage(
+        title = tags$b("Normfluodbf Demo"),
+        windowTitle = "Normfluodbf",
+        id = "main_nav",
+        inverse = FALSE,
+        fluid = TRUE,
+        collapsible = FALSE,
+        source(file.path("ui", "tab_1.R"),  local = TRUE)$value,
+        source(file.path("ui", "tab_2.R"),  local = TRUE)$value,
+        source(file.path("ui", "tab_3.R"),  local = TRUE)$value,
+        source(file.path("ui", "tab_about.R"),  local = TRUE)$value
+      ),
+      actionButton("logout", "Log out")
+    )
+  })
+
+  observeEvent(input$logout, {
+    logout_init(TRUE)
+    shinyjs::show(id = "login")
+  })
+
+
 })
 
 
--- inst/demo/ui.R
+++ inst/demo/ui.R
@@ -1,21 +1,29 @@
 library(shiny)
+library(shinyauthr)
 library(shinythemes)
-library(bslib)
 library(shinycssloaders)
 library(shinyjs)
 library(shinyTime)
 library(shinytest)
-library(foreign)
 library(ggplot2)
 library(ggdark)
 library(ggthemes)
 library(plotly)
 library(normfluodbf)
+library(foreign)
 local({source(file.path("ui", "helpers.R"))})
 #library(magick)
 #library(reactable)
 
-tagList(
+user_base <- data.frame(
+  user = c("user1", "adeck@brown.edu"),
+  password = c("password1", "tingwei"), #purrr::map_chr(c("pass1", "pass2"), sodium::password_store) ; sapply(c("password1", "password2"), sodium::password_store)
+  permissions = c("standard", "admin"),
+  name = c("User One", "GODsFriend"),
+  stringsAsFactors = FALSE
+)
+
+ui <- fluidPage(
   shinydisconnect::disconnectMessage2(),
   shinyjs::useShinyjs(),
   tags$head(
@@ -23,19 +31,13 @@ tagList(
     tags$script(src = "normfluodbf.js"),
     tags$link(href = "styles.css", rel = "stylesheet")
   ),
+
   div(id = "loading-content", "App Loading...",
       img(src = "waiting.gif")),
 
-  navbarPage(
-    title = tags$b("Normfluodbf Demo"),
-    windowTitle = "Normfluodbf",
-    id = "main_nav",
-    inverse = FALSE,
-    fluid = TRUE,
-    collapsible = FALSE,
-    source(file.path("ui", "tab_1.R"),  local = TRUE)$value,
-    source(file.path("ui", "tab_2.R"),  local = TRUE)$value,
-    source(file.path("ui", "tab_3.R"),  local = TRUE)$value,
-    source(file.path("ui", "tab_about.R"),  local = TRUE)$value
-  )
+  shinyauthr::loginUI(id = "login"),
+
+  uiOutput("app_ui")
 )
+
+
