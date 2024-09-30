7f2d501aac174ddbe2ade6810a283bd86d41a4d3
alphaprime7 <awesome.tingwei@outlook.com>
2024-09-30 13:47:28 -0500

    Initial commit

--- inst/shiny/demo/server/tab_2.R
+++ inst/shiny/demo/server/tab_2.R
@@ -43,7 +43,7 @@ output$download_data <- downloadHandler(
   filename = function() {
     paste0(input$dbfordat, ".csv")},
   content = function(file){
-    write.csv(editable_data(), file)})
+    write.csv(file_input(), file)})
 
 
 
